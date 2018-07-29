f32620eaa5cedc84f853538677f5a70884214d92
kotarou_nishio <sonnabakananz@gmail.com>
2018-07-29 02:33:16 +0000

    session作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/sign_in',　to: 'sessions#new'
+  get 'mypage', to: 'users#show'
 end
